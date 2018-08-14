8dc8d7708059aa7f5d77033fd619a24f687018a9
Tong Yu <rachieyu@gmail.com>
2018-08-14 16:56:58 +1000

    added sessions controlloer and login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   resources :catergories
   root "users#index"
-  get '/users/login', to: 'users#login'
   resources :requests
   resources :interests
   resources :skills
   resources :users
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
