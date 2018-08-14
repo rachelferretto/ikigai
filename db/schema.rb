c71f58121604d600401dbc5233d931e9a0187c0d
Rachel Ferretto <rachel.ferretto@rea-group.com>
2018-08-14 15:23:44 +1000

    drop duplicate table-categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_020959) do
+ActiveRecord::Schema.define(version: 2018_08_14_051900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "categories", force: :cascade do |t|
     t.text "name"
@@ -33,7 +32,7 @@ ActiveRecord::Schema.define(version: 2018_08_14_020959) do
   end
 
   create_table "requests", force: :cascade do |t|
-    t.text "keyword"
+    t.text "title"
     t.bigint "user_id"
     t.integer "mentor_id"
     t.text "request_message"
@@ -54,7 +53,7 @@ ActiveRecord::Schema.define(version: 2018_08_14_020959) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "firtname"
+    t.string "firstname"
     t.string "lastname"
     t.string "email"
     t.string "password_digest"
@@ -68,5 +67,4 @@ ActiveRecord::Schema.define(version: 2018_08_14_020959) do
   add_foreign_key "interests", "users"
   add_foreign_key "requests", "users"
   add_foreign_key "skills", "users"
-
 end
