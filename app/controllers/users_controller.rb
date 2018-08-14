5ea358790bfb349b399f4bfbcf41b0cca6f2f83d
Tong Yu <rachieyu@gmail.com>
2018-08-14 20:12:42 +1000

    sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
-    def login
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.email_taken?
+            flash[:notice] = ["email already exists"]
+            render 'new'
+        elsif @user.save == false
+            flash[:notice] = @user.errors.full_messages
+            render 'new'
+        else 
+            @user.save
+            flash[:success] = "Welcome to the Sample App!"
+            redirect_to @user
+        end
+        # add flash[:success] to user/id/show page after!!!
+    end
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+
+    def email_taken?
+        self.class.exists?(:email => email)
+    end
 end
