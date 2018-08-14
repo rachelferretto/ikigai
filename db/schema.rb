b21479c4d1e9a3df5da514fb3936eb8ecbf4867f
Rachel Ferretto <rachel.ferretto@rea-group.com>
2018-08-14 14:19:30 +1000

    seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_002234) do
+ActiveRecord::Schema.define(version: 2018_08_14_020959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.text "name"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "interests", force: :cascade do |t|
     t.text "keyword"
