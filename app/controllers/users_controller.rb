b15f3cd2cad4f49568766ca54077958060c67216
Tong Yu <rachieyu@gmail.com>
2018-08-15 15:03:20 +1000

    select skills on signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,26 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         @categories = Category.all
+        @skills = params[:skills]
+        p @skills
         if @user.email_taken?
             flash[:notice] = ["email already exists"]
             render 'new'
         elsif @user.save == false
+
+
             flash[:notice] = @user.errors.full_messages
             render 'new'
         else 
-            @user.save
-            
+            if @skills
+                @user.save
+                @skills.each do |skill|
+                    new_skill = Skill.new
+                    new_skill.category = skill
+                    new_skill.user = @user
+                    new_skill.save
+                end
+            end
             flash[:success] = "Welcome to the Sample App!"
             redirect_to @user
         end
@@ -39,4 +50,7 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+
+
 end
