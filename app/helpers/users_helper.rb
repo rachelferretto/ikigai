8f5bf83297a23aa2fc779bdcd2dfd0354894afac
Inder <inderyyz@gmail.com>
2018-08-15 15:07:58 +1000

    logout helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,6 +7,14 @@ module UsersHelper
     # double negation for current_user to boolean vs. if then true and false
     !!current_user #user object or nil
   end
+
+  delete '/session' do
+    #delete the session
+    session[:user_id] = nil
+    #redirect to /login
+    redirect '/'
+  end
+
 end
 
 
